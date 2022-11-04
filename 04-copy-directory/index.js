48848a24c0cfba716727b877ef54641f68f254de
Vitali Ahel <nonsens_bill@mail.ru>
2022-11-04 22:52:15 +0300

    docs: finish task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
-
+const fs = require('fs');
+const path = require('path');
+fs.mkdir(path.join(__dirname, 'files-copy'),{recursive: true}, (error) => {
+    if (error) return console.error(error.message)
+});
+fs.readdir(path.join(__dirname,'files'), (error, files) => {
+    if (error) return console.error(error.message);    
+    for (let i = 0; i < files.length; i++) {
+      fs.copyFile(path.join(__dirname,'files',files[i]), path.join(__dirname, 'files-copy',files[i]), (error) =>{
+        if (error) return console.error(error.message)
+      })
+       }
+});
+        
+        
+    
+    
+    
