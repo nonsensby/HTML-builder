ad4a72b8523ebfea3a5aa0f1413d79ccad2f49a4
HunterTigerX <HunterTigerX@gmail.com>
2022-11-08 00:39:30 +0300

    fix: fix bugs

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,14 +7,15 @@ const { stdin, stdout } = process;
 stdout.write("Приглашение на ввод текста\n");
 stdin.on("data", (data) => {
   fs.appendFile(`${folder}\\yourMessages.txt`, data.toString(), (err) => {
-    if (err) {
-      console.log(err);
-    }
   });
+  if (data.toString("utf8").trim() === "exit") {
+    process.exit();
+  }
 });
 
-process.on("exit", () => stdout.write("Прощальная фраза. Процесс завершён."));
-process.on("SIGINT", () => {
+process.on("exit", () => {
   stdout.write("Прощальная фраза. Процесс завершён.");
+});
+process.on("SIGINT", () => {
   process.exit();
 });
