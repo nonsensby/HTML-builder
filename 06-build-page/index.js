f69b996b61459b7866c432c89c86391a85d61bb5
HunterTigerX <HunterTigerX@gmail.com>
2022-11-08 15:05:45 +0300

    fix: fix task 6 error

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -9,105 +9,104 @@ const fsPromises = require("fs").promises;
 
 /*1. Создаёт папку  **project-dist**.*/
 fs.mkdir(folderProjectDist, (err) => {
-});
-fs.mkdir(folderProjectDistAssets, (err) => {
-});
+  fs.mkdir(folderProjectDistAssets, (err) => {
+    fs.writeFile(`${folderProjectDist}\\style.css`, "", (err) => {});
 
-fs.writeFile(`${folderProjectDist}\\style.css`, "", (err) => {});
+    /*4. Копирует папку **assets** в **project-dist/assets***/
+    function copyNewFolder(destination, folderName) {
+      fs.readdir(destination, (err, files) => {
+        files.forEach((file) => {
+          fs.stat(path.join(destination, file), (error, stats) => {
+            let pathFull = path.join(destination, file);
+            let lengthOfOld = folderAssets.length;
+            let sliced = pathFull.slice(lengthOfOld);
+            let newFolder = path.join(folderProjectDistAssets, sliced);
+            if (stats.isDirectory()) {
+              fs.mkdir(newFolder, (err) => {});
+              copyNewFolder(path.join(destination, file), file);
+            } else if (stats.isFile()) {
+              fsPromises.copyFile(`${destination}//${file}`, `${newFolder}`);
+            }
+          });
+        });
+      });
+    }
 
-/*4. Копирует папку **assets** в **project-dist/assets***/
-function copyNewFolder(destination, folderName) {
-  fs.readdir(destination, (err, files) => {
-    files.forEach((file) => {
-      fs.stat(path.join(destination, file), (error, stats) => {
-        let pathFull = path.join(destination, file);
-        let lengthOfOld = folderAssets.length;
-        let sliced = pathFull.slice(lengthOfOld);
-        let newFolder = path.join(folderProjectDistAssets, sliced);
-        if (stats.isDirectory()) {
-          fs.mkdir(newFolder, (err) => {});
-          copyNewFolder(path.join(destination, file), file);
-        } else if (stats.isFile()) {
-          fsPromises.copyFile(`${destination}//${file}`, `${newFolder}`);
+    copyNewFolder(folderAssets);
+
+    /*3. Собирает в единый файл стили из папки **styles** и помещает их в файл **project-dist/style.css**.*/
+    fs.readdir(folderCss, (err, files) => {
+      files.forEach((file) => {
+        fileExtention = file.split(".")[1];
+        if (fileExtention === "css") {
+          fs.readFile(`${folderCss}\\${file}`, (err, data) => {
+            fs.appendFile(`${folderProjectDist}\\style.css`, data, (err) => {});
+          });
         }
       });
     });
-  });
-}
 
-copyNewFolder(folderAssets);
+    /*2. Заменяет шаблонные теги в файле **template.html** с названиями файлов из папки components (пример:```{{section}}```) на содержимое одноимённых компонентов и сохраняет результат в **project-dist/index.html**.*/
+    fs.copyFile(
+      `${__dirname}\\template.html`,
+      `${folderProjectDist}\\index.html`,
+      (err) => {}
+    );
 
-/*3. Собирает в единый файл стили из папки **styles** и помещает их в файл **project-dist/style.css**.*/
-fs.readdir(folderCss, (err, files) => {
-  files.forEach((file) => {
-    fileExtention = file.split(".")[1];
-    if (fileExtention === "css") {
-      fs.readFile(`${folderCss}\\${file}`, (err, data) => {
-        fs.appendFile(`${folderProjectDist}\\style.css`, data, (err) => {});
-      });
-    }
-  });
-});
+    let result = "";
 
-/*2. Заменяет шаблонные теги в файле **template.html** с названиями файлов из папки components (пример:```{{section}}```) на содержимое одноимённых компонентов и сохраняет результат в **project-dist/index.html**.*/
-fs.copyFile(
-  `${__dirname}\\template.html`,
-  `${folderProjectDist}\\index.html`,
-  (err) => {}
-);
-
-let result = "";
-
-const readableStream = fs.ReadStream(`${folderProjectDist}\\index.html`);
-readableStream.on("readable", function () {
-  let fileText = readableStream.read();
-  if (fileText != null) {
-    result += fileText;
-  }
-});
-
-readableStream.on("end", function () {
-  fs.readdir(folderComponents, (err, files) => {
-    files.forEach((file) => {
-      fileName = file.split(".")[0];
-      fileExtention = file.split(".")[1];
-      if (fileExtention === "html") {
-        targetFile = `${folderProjectDist}\\index.html`; /*Шаблон*/
-        checkIfContains(file, fileName);
+    const readableStream = fs.ReadStream(`${folderProjectDist}\\index.html`);
+    readableStream.on("readable", function () {
+      let fileText = readableStream.read();
+      if (fileText != null) {
+        result += fileText;
       }
     });
-  });
-});
 
-async function checkIfContains(file, fileName) {
-  file = path.join(folderComponents, file); /*Вставляемый файл*/
-  targetFile = `${folderProjectDist}\\index.html`; /*Шаблон*/
+    readableStream.on("end", function () {
+      fs.readdir(folderComponents, (err, files) => {
+        files.forEach((file) => {
+          fileName = file.split(".")[0];
+          fileExtention = file.split(".")[1];
+          if (fileExtention === "html") {
+            targetFile = `${folderProjectDist}\\index.html`; /*Шаблон*/
+            checkIfContains(file, fileName);
+          }
+        });
+      });
+    });
 
-  fs.readFile(`${targetFile}`, "utf8", function (err, data) {
-    if (data.includes(`{{${fileName}}}`)) {
-      fs.readFile(`${file}`, "utf8", function (err, data2) {
-        replace(data2, fileName);
-        checkNewIndex(data, data2, fileName);
+    async function checkIfContains(file, fileName) {
+      file = path.join(folderComponents, file); /*Вставляемый файл*/
+      targetFile = `${folderProjectDist}\\index.html`; /*Шаблон*/
+
+      fs.readFile(`${targetFile}`, "utf8", function (err, data) {
+        if (data.includes(`{{${fileName}}}`)) {
+          fs.readFile(`${file}`, "utf8", function (err, data2) {
+            replace(data2, fileName);
+            checkNewIndex(data, data2, fileName);
+          });
+        }
       });
     }
+
+    function replace(data2, fileName) {
+      result = result.replace(`{{${fileName}}}`, data2);
+      fs.writeFile(`${folderProjectDist}\\index.html`, result, (err) => {});
+      return new Promise((resolve) => {
+        resolve("Done");
+      });
+    }
+
+    async function checkNewIndex(data, data2, fileName) {
+      let check = await replace(data, data2, fileName);
+      if (check === "Done") {
+        writeNewIndex();
+      }
+    }
+
+    async function writeNewIndex() {
+      fs.writeFile(`${folderProjectDist}\\index.html`, result, (err) => {});
+    }
   });
-}
-
-function replace(data2, fileName) {
-  result = result.replace(`{{${fileName}}}`, data2);
-  fs.writeFile(`${folderProjectDist}\\index.html`, result, (err) => {});
-  return new Promise((resolve) => {
-    resolve("Done");
-  });
-}
-
-async function checkNewIndex(data, data2, fileName) {
-  let check = await replace(data, data2, fileName);
-  if (check === "Done") {
-    writeNewIndex();
-  }
-}
-
-async function writeNewIndex() {
-  fs.writeFile(`${folderProjectDist}\\index.html`, result, (err) => {});
-}
+});
